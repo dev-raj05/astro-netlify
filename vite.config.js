edc7e3a8c0563969b818cb9c5de1a7ba6b4b5e76
dev-raj05 <dev.dhanak@medront.com>
2025-02-19 13:21:06 +0530

    host added

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,7 @@
-// // vite.config.js
-// import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 
-// export default defineConfig({
-//   optimizeDeps: {
-//     esbuildOptions: {
-//       plugins: [
-//         {
-//           name: "fix-shallowequal",
-//           setup(build) {
-//             // Intercept imports of "shallowequal"
-//             build.onResolve({ filter: /^shallowequal$/ }, (args) => {
-//               return { path: args.path, namespace: "fix-shallowequal" };
-//             });
-//             // Re-export everything from "shallowequal" as a default export
-//             build.onLoad(
-//               { filter: /.*/, namespace: "fix-shallowequal" },
-//               async () => {
-//                 return {
-//                   contents:
-//                     'import * as se from "shallowequal"; export default se;',
-//                   loader: "js",
-//                 };
-//               }
-//             );
-//           },
-//         },
-//       ],
-//     },
-//   },
-// });
+export default defineConfig({
+  server: {
+    allowedHosts: ["devserver.booleanmaths.com"],
+  },
+});
